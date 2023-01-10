6509e32ab1551bf085ff6f7abad4151fae6da193
omnia goher <omniagoher99@gmail.com>
2023-01-10 14:04:13 +0200

    JS lab1

---
+++ task1/script.js
@@ -0,0 +1,12 @@
+
+
+let f_name = prompt("Enter your first name : ")
+let l_name = prompt("Enter your last name : ")
+
+let full_name = f_name + " " + l_name
+
+let b_year = Number(prompt("Enter your birth year : "))
+
+let age = 2023 - b_year
+
+alert(`Welcome ${full_name} you are ${age} years old`)
---
+++ task2/script.js
@@ -0,0 +1,11 @@
+let msg = "this a simple calculator for summation"
+
+alert(msg)
+
+let no1 = Number(prompt("Enter first number : "))
+
+let no2 = Number(prompt("Enter second number : "))
+
+let sum  = no1 + no2
+
+alert(`${no1} + ${no2} = ${sum}`)
